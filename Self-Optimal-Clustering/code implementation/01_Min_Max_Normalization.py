1db476386d32c49f7f6f4fea50df5a8e603a112f
mehtapriyanshu172006-droid <mehtapriyanshu172006@gmail.com>
2026-06-30 00:30:18 +0530

    Update 01_Min_Max_Normalization.py

--- Self-Optimal-Clustering/code implementation/01_Min_Max_Normalization.py
+++ Self-Optimal-Clustering/code implementation/01_Min_Max_Normalization.py
@@ -1,1 +1,6 @@
-
+def min_max_normalize(X):
+    X_min = np.min(X, axis=0)
+    X_max = np.max(X, axis=0)
+    denominator = X_max - X_min
+    denominator[denominator == 0] = 1e-10
+    return (X - X_min) / denominator
